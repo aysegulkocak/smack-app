51b3c6af0e3dcb4d6f4260c5025e8aa1f38791b6
Ayşegül Koçak <aysegulkocak@Aysegul-MacBook-Pro.local>
2018-07-22 00:05:59 +0300

    Lesson 9

---
+++ smack-app/Services/AuthService.swift
@@ -0,0 +1,71 @@
+//
+//  AuthService.swift
+//  smack-app
+//
+//  Created by Ayşegül Koçak on 21.07.2018.
+//  Copyright © 2018 Ayşegül Koçak. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService() // singleton
+    let defaults = UserDefaults.standard
+    
+    
+    // 3 of service variables
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken : String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail : String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            }
+            else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+            
+        }
+        
+    }
+    
+}
--- smack-app/Utilities/Constants.swift
+++ smack-app/Utilities/Constants.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> () // typealies is simply renaming a type. This type is "closure"
+/*
+ What a closure is it's a first class function that can be passed around in code.
+ */
+
+//URL Constants
+let BASE_URL = "https://chattychatychatchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
