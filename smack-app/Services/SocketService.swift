e4facfb039861d1d70f07a96fcbb00ad957fa933
Ayşegül Koçak <aysegulkocak@Aysegul-MacBook-Pro.local>
2018-08-25 00:26:12 +0300

    This was the lagendary

---
+++ smack-app/Services/SocketService.swift
@@ -0,0 +1,13 @@
+//
+//  SocketService.swift
+//  smack-app
+//
+//  Created by Ayşegül Koçak on 24.08.2018.
+//  Copyright © 2018 Ayşegül Koçak. All rights reserved.
+//
+
+import UIKit
+
+class SocketService: NSObject {
+
+}
---
+++ smack-app/View/ChannelCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChannelCell.swift
+//  smack-app
+//
+//  Created by Ayşegül Koçak on 21.08.2018.
+//  Copyright © 2018 Ayşegül Koçak. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
