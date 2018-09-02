f7dc884a839792792d0a8dddc18745e9df10d76b
Ayşegül Koçak <aysegulkocak@Aysegul-MacBook-Pro.local>
2018-09-02 06:51:10 +0300

    Lesson-25

--- smack-app/Controller/ChatVC.swift
+++ smack-app/Controller/ChatVC.swift
@@ -13,9 +13,15 @@ class ChatVC: UIViewController {
     //Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLabel: UILabel!
+    @IBOutlet weak var messageTextBox: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.bindToKeyboard() // making the keyboard move
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
+        
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -48,11 +54,33 @@ class ChatVC: UIViewController {
         updateWithChannel()
     }
     
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLabel.text = "#\(channelName)"
         getMessages()
     }
+    
+    @IBAction func sendMsgPressed(_ sender: Any) {
+        
+//  We were trying to send a message that message is going to travel to the API with its all of its message information. The app is going to receive that message and construct a new message object and then send that information back to our app as well as any other apps that happen to be connected at that point in time.
+        
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTextBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId, completion:  { (success) in
+                if success {
+                    self.messageTextBox.text = ""
+                    self.messageTextBox.resignFirstResponder()
+                }
+            })
+        }
+    }
+    
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
@@ -71,7 +99,6 @@ class ChatVC: UIViewController {
     func getMessages() {
         guard let channelId = MessageService.instance.selectedChannel?.id else { return }
         MessageService.instance.findAllMessageForChannel(channelId: channelId) { (success) in
-            
         }
     }
 
---
+++ smack-app/View/KeyboardBoundView.swift
@@ -0,0 +1,33 @@
+//
+//  KeyboardBoundView.swift
+//  SmackAlpha
+//
+//  Created by Jonny B on 7/11/17.
+//  Copyright © 2017 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func bindToKeyboard(){
+        NotificationCenter.default.addObserver(self, selector: #selector(UIView.keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let curFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let targetFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = targetFrame.origin.y - curFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+            
+        },completion: {(true) in
+            self.layoutIfNeeded()
+        })
+    }
+}
+// What we can do is we combined any of view to the keyboard so that when we click on the text box and the
+// keyboard opens up we can say we can have it so that the the view will shift up with it.
